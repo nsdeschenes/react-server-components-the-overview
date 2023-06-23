2677d676c12b7e256451184f6508e1411909f87b
nsdeschenes <nsdeschenes@outlook.com>
2023-06-23 10:25:20 -0300

    move some deps over to the server

--- react-server-components/src/app/dependencies-on-the-server/page.tsx
+++ react-server-components/src/app/dependencies-on-the-server/page.tsx
@@ -1,11 +1,53 @@
+import { marked } from "marked";
+import sanitizeHTML from "sanitize-html";
+
 // force component to be server-side rendered
 export const revalidate = 0;
 
+const markdown = `
+## Dependencies on the Server
+
+Server components can use any Node.js package, including packages that rely on native code. For example, you can use \`sharp\` to resize images on the server.
+
+\`\`\`jsx
+"use client";
+
+function CoolComponent() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button onClick={() => {
+      setCount(count + 1)
+    }}>
+      Count: {count}
+    </button>
+  )
+};
+\`\`\`
+`;
+
 export default function DependenciesOnTheServer() {
+  const parsedMarkdown = marked.parse(markdown, {
+    mangle: false,
+    headerIds: false,
+  });
+  const html = sanitizeHTML(parsedMarkdown);
+
   return (
     <div>
       <h1>Dependencies on the Server</h1>
-      <p>Coming soon...</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <article
+          style={{ width: "40%", textAlign: "left" }}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
     </div>
   );
 }
