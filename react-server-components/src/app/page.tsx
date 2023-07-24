cc50180ed4086bf3773bf0671be59403244d117c
nsdeschenes <nsdeschenes@outlook.com>
2023-07-24 15:50:20 -0300

    use cookie button in page

--- react-server-components/src/app/page.tsx
+++ react-server-components/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Button from "./_Button/Button";
+import CookieButton from "./_CookieButton/Button";
 import Marquee from "./_Marquee/Marquee";
 
 export default function Home() {
@@ -7,6 +8,8 @@ export default function Home() {
       <h1>React Server Components</h1>
       <Marquee />
       <Button />
+      <br />
+      <CookieButton />
     </div>
   );
 }
